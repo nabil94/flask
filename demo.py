5797e5629ea515c9f2f8d59496e9cdfdd2ddad79
nabil94 <38067626+nabil94@users.noreply.github.com>
2022-04-21 13:09:47 +0600

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,49 @@
+# Issue 30
+
+class A:
+    def foo(self):
+        buzz()
+        print('inside class A')
+        
+class A1:
+    def foo(self):
+        buzz()
+        print('inside class A')
+
+class B(A):
+    def foo(self):
+        print('inside class B')
+ 
+        
+class K(A):
+    def foo(self):
+        print('inside class K')
+         
+class C(B):
+    def foo(self):
+        print('inside class C')
+
+    def bar(self):
+        self.foo()
+        print('inside bar')
+        
+class C1(B):
+    def foo(self):
+        print('inside class C')
+
+    def bar(self):
+        self.foo()
+        print('inside bar')
+
+def driver():
+    a = A()
+    a.foo()
+    
+def plus(x):
+    return x+1
+
+def minus(x):
+    return x-1
+
+def mul(x, y):
+    return x*y
